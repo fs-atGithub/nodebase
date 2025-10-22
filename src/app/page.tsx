969ddc7b3baa11ad52325461b293bc488505058c
fs-atGithub <filip.sostaric@protonmail.com>
2025-10-22 18:08:48 +0200

    prefetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import prisma from "@/lib/db";
+import React, {Suspense} from 'react'
+import {getQueryClient, trpc} from "@/trpc/server"
+import {Client} from "@/app/clent";
+import {HydrationBoundary, dehydrate} from "@tanstack/react-query";
+
+
 
 const Page = async () => {
-    const users = await  prisma.user.findMany()
+    const queryClient = getQueryClient()
+    void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
     return (
         <div className={"flex min-h-screen min-w-screen justify-center items-center" }>
-            {JSON.stringify(users, null, 2)}
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<p>Loading...</p>}>
+                <Client />
+            </Suspense>
+            </HydrationBoundary>
+
         </div>
     )
 }
 export default Page
+
+
